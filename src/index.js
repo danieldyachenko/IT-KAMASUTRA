d48ccefa44410260d043994bef748baea20c2fff
danieldyachenko <daniildyachenko95@gmail.com>
2020-02-20 23:47:53 +0300

    -index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import store from './redux/redux-store.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App.js';
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from 'react-redux'
