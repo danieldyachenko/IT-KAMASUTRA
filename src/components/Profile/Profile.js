e1255a85ef27e6dbdce5f2ff2858f454a34bf93d
danieldyachenko <daniildyachenko95@gmail.com>
2020-02-21 21:06:40 +0300

    no

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './Profile.module.css'
 import MyPostsContainer from './MyPosts/MyPostsContainer';
-import ProfileInfo from './ProfileInfo/ProfileInfo';
+import ProfileInfo from './ProfileInfo/ProfileInfo.jsx';
 
 const Profile = (props) => {
   return (
--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -25,5 +25,5 @@ let mapStateToProps = (state) => ({
 export default compose(
     connect( mapStateToProps, {getUserProfile} ),
     withRouter,
-    withAuthRedirect
+    //withAuthRedirect
 )(ProfileContainer);
