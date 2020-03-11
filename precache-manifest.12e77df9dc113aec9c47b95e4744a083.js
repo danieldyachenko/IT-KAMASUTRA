85530019a868376cfe2ea8fc9df783686d2ef56b
danieldyachenko <daniildyachenko95@gmail.com>
2020-03-11 18:26:39 +0300

    Updates

---
+++ precache-manifest.12e77df9dc113aec9c47b95e4744a083.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f983e2a564744f8a961dd093e4514625",
+    "url": "/IT-KAMASUTRA/index.html"
+  },
+  {
+    "revision": "116035a646f7339612a1",
+    "url": "/IT-KAMASUTRA/static/css/3.19ba7e4e.chunk.css"
+  },
+  {
+    "revision": "78b940ed88e42a154c3d",
+    "url": "/IT-KAMASUTRA/static/css/main.1ed089c2.chunk.css"
+  },
+  {
+    "revision": "edf196ae699ca1249eed",
+    "url": "/IT-KAMASUTRA/static/js/2.2192b2bc.chunk.js"
+  },
+  {
+    "revision": "116035a646f7339612a1",
+    "url": "/IT-KAMASUTRA/static/js/3.622acbd2.chunk.js"
+  },
+  {
+    "revision": "78b940ed88e42a154c3d",
+    "url": "/IT-KAMASUTRA/static/js/main.66441605.chunk.js"
+  },
+  {
+    "revision": "44e9d5bf5cbe30d331e2",
+    "url": "/IT-KAMASUTRA/static/js/runtime-main.36cc4e88.js"
+  },
+  {
+    "revision": "6dc0808de061bce5ecd01ef5b3debcc3",
+    "url": "/IT-KAMASUTRA/static/media/preloader.6dc0808d.gif"
+  },
+  {
+    "revision": "d48a8973a98231204ed9be58c6095785",
+    "url": "/IT-KAMASUTRA/static/media/user.d48a8973.png"
+  }
+]);
