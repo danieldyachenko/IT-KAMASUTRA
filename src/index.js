093e3b4215a8f0968a43d02fb53a95b058196445
danieldyachenko <daniildyachenko95@gmail.com>
2020-03-11 19:13:35 +0300

    history api

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import store from './redux/redux-store.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 import {Provider} from "react-redux";
 
 ReactDOM.render (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>, document.getElementById('root')
+    </HashRouter>, document.getElementById('root')
 );
